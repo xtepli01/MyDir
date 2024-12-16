9fce6d00f72b1b3d5924ee41439bd1c36bb04675
xtepli01 <al.tepl@icloud.com>
2024-12-16 20:46:28 +0200

    task1_complete

--- cs/cpp/task1.cpp
+++ cs/cpp/task1.cpp
@@ -1,15 +1,20 @@
+// Task1: Input quantity of seconds. Targer- calculate how many hours, minutes, seconds.
+
 #include <iostream>
 using namespace std;
 
 int main(){
 
-    int n;
-    cin >> n;
-
-    int h, m1, m2, s1, s2;
-
-    h = ( n % 3600) / 24;
-
-    cout << h;
+    int seconds;
+    cin >> seconds;
+    
+    int hour = (seconds / 3600) % 24;
+    int min1 = ((seconds % 3600) / 600);
+    int min2 = ((seconds % 3600) % 600) / 60;
+    int sec1 = ((seconds % 60) / 10);
+    int sec2 = ((seconds % 60) % 10);
+    
+    cout << hour << ":" << min1 << min2 << ":"  << sec1 << sec2;
+    
     return 0;
 }
